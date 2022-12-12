5612fc5ab689a57218afeb8ccf5db6e6e72fdaa6
Prabodh Mahajan <prabodh0201@gmail.com>
2022-12-12 15:01:37 +0530

    Add files via upload

---
+++ 3. Variables in JS.js
@@ -0,0 +1,29 @@
+//Using variables
+// Identifying what a variable is and why we need it
+// Distinguish between variable declaration & variable assignment
+// Describe how to get a variable value & how to reassign it
+
+
+// Assigning the value to a variable & output it
+//Console in browser - F12 to program in browser
+
+// To declare a variable we use var (var allows you to save a variable so that you can re-use it in future)
+var person; //variable declaration but has not been assigned any value
+
+//Assigning value to variable
+var person = "Prabodh"
+
+//Outputting value in browser/ console
+console.log("hello", person)
+
+var greeting = "hello"
+console.log(greeting, person)
+
+//changing the variable values - You don't need to use keyword var now for the defined variables
+person = "Ram"
+greeting = "hi"
+
+console.log(greeting, person)
+console.log(greeting, person)
+
+
